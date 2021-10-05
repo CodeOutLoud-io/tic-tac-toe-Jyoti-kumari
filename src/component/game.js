145f75771c624e8d9b928462a90f0e594ce4c36b
Jyoti kumari <jaiswaljyoti2000@gmail.com>
2021-10-05 20:16:23 +0530

    created a square board

--- src/component/game.js
+++ src/component/game.js
@@ -1,14 +1,28 @@
-import react,{Component} from 'react';
+import React,{ Component } from 'react';
+import Board from './Board';
 
 export default class Game extends Component{
     constructor(props){
         super(props);
+        this.state =  {
+            xIsNext: true,
+            stepNumber: 0,
+            history: [
+                {
+                    squares: Array(9).fill(null)
+                }
+            ]
+        }
 
     }
     render(){
         return(
-            <div>
-                
+            <div className="game">
+                <div className="game-board">
+                    <Board onClick = {(i) =>this.handleClick(i)}
+                    squares = {current.squares}
+                    />
+                </div>
             </div>
         )
     }
