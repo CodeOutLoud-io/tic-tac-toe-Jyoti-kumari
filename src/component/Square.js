fd1ddc589e2b089e3b0a2a341b08bd5d0785a39d
Jyoti kumari <jaiswaljyoti2000@gmail.com>
2021-10-06 14:10:37 +0530

    tic-tac-toe game is ready to play

--- src/component/Square.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function square(props){
-    return(
-        <button className="square" onClick = {props.onClick}>
-            {props.value}
-        </button>
-    )
-}
